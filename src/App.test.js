efe2a3bb78c63b391514b7be7fefc08fe24bb176
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2021-07-31 21:34:18 +0300

    Checkbox updated

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import Home from './Home';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<Home />, div);
+  //ReactDOM.unmountComponentAtNode(div);
+
+
+  expect(div.querySelector("h4").textContent).toBe("Home");
 });
+test('h4', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+
+
+  expect(div.querySelector("h4").textContent).toBe("Home");
+});
+
+
--- src/Home.js
+++ src/Home.js
@@ -2,45 +2,85 @@ import React from "react";
 import app from "./base";
 
 import "./Home.css"
+import  CheckBox  from './CheckBox'
 
-const Home = () => {
-    return (
+
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: '',
+            gender: '',
+            symptoms: [
+                {id: 1, value: "fever", isChecked: false},
+                {id: 2, value: "dry cough", isChecked: false},
+                {id: 3, value: "tiredness", isChecked: false},
+                {id: 4, value: "aches and pains", isChecked: false},
+                {id: 5, value: "sore throat", isChecked: false},
+                {id: 6, value: "diarrhoea", isChecked: false},
+                {id: 7, value: "conjunctivitis", isChecked: false},
+                {id: 8, value: "headache", isChecked: false},
+                {id: 9, value: "loss of taste or smell", isChecked: false},
+                {id: 10, value: "difficulty breathing or shortness of breath", isChecked: false},
+                {id: 11, value: "chest pain or pressure", isChecked: false},
+                {id: 12, value: "loss of speech or movement", isChecked: false},
+            ],
+        };
+    }
+
+    ageHandler = (event) => {
+        this.setState({age: event.target.value});
+    }
+    genderHandler = (event) => {
+        this.setState({gender: event.target.value});
+    }
+    handleAllChecked = (event) => {
+        let symptoms = this.state.symptoms
+        symptoms.forEach(symptom => symptom.isChecked = event.target.checked)
+        this.setState({symptoms: symptoms})
+    }
+
+    handleCheckChildElement = (event) => {
+        let symptoms = this.state.symptoms
+        symptoms.forEach(symptom => {
+            if (symptom.value === event.target.value)
+                symptom.isChecked =  event.target.checked
+        })
+        this.setState({symptoms: symptoms})
+    }
+
+
+    render() {
+        return (
             <div className="home-page">
-                <h5>Home</h5>
+                <h4>Home</h4>
                 <h5>COVID MONITORING PAGE</h5>
                 <form>
                     <label>
                         Age
-                        <input name="age" type="age" placeholder="Age"/>
+                        <input onChange={this.ageHandler} name="age" type="age" placeholder="Age"/>
                     </label>
                     <label>
                         Gender
-                        <input name="gender" type="gender" placeholder="Gender"/>
+                        <input onChange={this.genderHandler} name="gender" type="gender" placeholder="Gender"/>
                     </label>
-                    <div className="checkboxes">
-                        <label>Common Symptoms</label>
-                        <label><span>fever</span><input type="checkbox"/></label>
-                        <label><span>dry cough</span><input type="checkbox"/></label>
-                        <label><span>tiredness</span><input type="checkbox"/></label>
-
-                        <label>Less Common Symptoms</label>
-                        <label><span>aches and pains</span><input type="checkbox"/></label>
-                        <label><span>sore throat</span><input type="checkbox"/></label>
-                        <label><span>diarrhoea</span><input type="checkbox"/></label>
-                        <label><span>conjunctivitis</span><input type="checkbox"/></label>
-                        <label><span>headache</span><input type="checkbox"/></label>
-                        <label><span>loss of taste or smell</span><input type="checkbox"/></label>
-
-                        <label>Serious Symptoms</label>
-                        <label><span>difficulty breathing or shortness of breath</span><input type="checkbox"/></label>
-                        <label><span>chest pain or pressure</span><input type="checkbox"/></label>
-                        <label><span>loss of speech or movement</span><input type="checkbox"/></label>
-
+                    <div className="checkSymptoms">
+                        <label>All Symptoms</label>
+                        <input type="checkbox" onChange={this.handleAllChecked}  value="checkedall" /> Check / Uncheck All
+                        <ul>
+                            {
+                                this.state.symptoms.map((symptom, index) => {
+                                    return (<CheckBox key={index} handleCheckChildElement={this.handleCheckChildElement}  {...symptom} />)
+                                })
+                            }
+                        </ul>
                     </div>
                 </form>
                 <button className="submitBtn" onClick={() => app.auth().signOut()}>Sign out</button>
             </div>
-    );
-};
+        );
+    }
 
+}
 export default Home;
